30a33c7839ed184d9f64c5e7ca4f336f4866d50f
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-05-12 15:08:42 +0930

    fix: data

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ setInterval(() => {
     try {
         const memoryData = process.memoryUsage();
         const memoryUsage = {
-            rss: `${memoryData.rss} -> Resident Set Size - total memory allocated for the process execution`,
-            heapTotal: `${memoryData.heapTotal} -> total size of the allocated heap`,
-            heapUsed: `${memoryData.heapUsed} -> actual memory used during the execution`,
-            external: `${memoryData.external} -> V8 external memory`,
+            rss: memoryData.rss, // -> Resident Set Size - total memory allocated for the process execution`,
+            heapTotal: memoryData.heapTotal, // -> total size of the allocated heap`,
+            heapUsed: memoryData.heapUsed, // -> actual memory used during the execution`,
+            external: memoryData.external, // -> V8 external memory`,
         };
         logger.info('Memory usage', { memoryUsage });
     } catch { }
